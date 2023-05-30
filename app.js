c63e828ee2ffba0164f9286387e08b707cbf4bd4
SebastiánTV <107218018+xlPlanetarizaGol@users.noreply.github.com>
2023-05-29 22:31:46 -0500

    Update app and add new objects.js

--- app.js
+++ app.js
@@ -2,67 +2,211 @@ let scene;
 let camera;
 let renderer;
 
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
 
-function main()
-{
-    const canvas = document.querySelector('#c');
+let mouthMesh;
+let isOpening = true;
+let mouthOpenAmount = 0;
 
+function main() {
+  const canvas = document.querySelector('#c');
 
-    scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 2;
-    scene.add(camera);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 2;
+  scene.add(camera);
 
-    renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true,});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
+  renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
 
-    renderer.autoClear = false;
-    renderer.setClearColor(0x00000, 0.0);
+  renderer.autoClear = false;
+  renderer.setClearColor(0x00000, 0.0);
 
+  const earthGeometry = new THREE.SphereGeometry(0.6, 32, 32);
 
-    // create earthgeometry
+  const earthMaterial = new THREE.MeshPhongMaterial({
+    map: THREE.ImageUtils.loadTexture('texture/earthmap1k.jpg'),
+    bumpMap: THREE.ImageUtils.loadTexture('texture/earthbump.jpg'),
+    bumpScale: 0.3,
+  });
 
-    const earthgeometry = new THREE.SphereGeometry(0.6,32,32);
+  const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
+  scene.add(earthMesh);
 
-    const eatrhmaterial = new THREE.MeshPhongMaterial({
-        roughness : 1,
-        metalness:0,
-        map: THREE.ImageUtils.loadTexture('texture/earthmap1k.jpg'),
-        bumpMap: THREE.ImageUtils.loadTexture('texture/earthbump.jpg'),
-        bumpScale: 0.3,
-    });
+  const sunGeometry = new THREE.SphereGeometry(0.2, 32, 32);
 
-    const earthmesh = new THREE.Mesh(earthgeometry,eatrhmaterial);
+  const sunMaterial = new THREE.MeshPhongMaterial({
+    map: THREE.ImageUtils.loadTexture('texture/sunback.jpg'),
+    side: THREE.FrontSide,
+    shininess: 0,
+  });
 
-    scene.add(earthmesh);
+  const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+  sunMesh.position.set(-1, 1, -1);
+  scene.add(sunMesh);
 
-    // set ambientlight
+  const pointerLight = new THREE.PointLight(0xffffff, 0.9);
 
-    // set point light
+  pointerLight.position.set(5, 3, 5);
+  scene.add(pointerLight);
 
-    const pointerlight =  new THREE.PointLight(0xffffff,0.9);
+  const starGeometry = new THREE.SphereGeometry(800, 64, 64);
 
-    // set light position
+  const starMaterial = new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('texture/galaxy.jpg'),
+    side: THREE.BackSide,
+  });
 
-    pointerlight.position.set(5,3,5);
-    scene.add(pointerlight);
+  const starMesh = new THREE.Mesh(starGeometry, starMaterial);
+  scene.add(starMesh);
 
+  earthMesh.position.set(0, 0, 0);
+  starMesh.position.set(0, 0, 0);
 
-    const animate = () =>{
-        requestAnimationFrame(animate);
-        earthmesh.rotation.y -= 0.0015;
+  const mouthTextures = [
+    'texture/boca1.png',
+    'texture/boca2.png',
+  ];
 
+  const mouthGeometry = new THREE.PlaneGeometry(1.2, 0.8);
+  const mouthMaterial = new THREE.MeshBasicMaterial({ transparent: true });
 
-        render();
+  mouthMesh = new THREE.Mesh(mouthGeometry, mouthMaterial);
+  scene.add(mouthMesh);
+
+  function handleMouseWheel(event) {
+    const delta = Math.sign(event.deltaY);
+
+    const zoomSpeed = 0.1;
+    camera.position.z -= delta * zoomSpeed;
+  }
+
+  canvas.addEventListener('wheel', handleMouseWheel);
+
+  function onKeyDown(event) {
+    switch (event.keyCode) {
+      case 38:
+        moveForward = true;
+        break;
+      case 40:
+        moveBackward = true;
+        break;
+      case 37:
+        moveLeft = true;
+        break;
+      case 39:
+        moveRight = true;
+        break;
     }
+  }
 
-    const render = () => {
-        renderer.render(scene,camera);
+  function onKeyUp(event) {
+    switch (event.keyCode) {
+      case 38:
+        moveForward = false;
+        break;
+      case 40:
+        moveBackward = false;
+        break;
+      case 37:
+        moveLeft = false;
+        break;
+      case 39:
+        moveRight = false;
+        break;
     }
+  }
 
-    animate();
+  document.addEventListener('keydown', onKeyDown);
+  document.addEventListener('keyup', onKeyUp);
+
+  function animate() {
+    requestAnimationFrame(animate);
+    updateCameraPosition();
+    earthMesh.rotation.y -= 0.0015;
+    sunMesh.rotation.y += 0.2;
+    starMesh.rotation.y += 0.0005;
+    moveRandomMouth();
+    render();
+  }
+
+  function render() {
+    renderer.clear();
+    renderer.render(scene, camera);
+  }
+
+  function updateCameraPosition() {
+    const moveSpeed = 0.05;
+
+    if (moveForward) {
+      camera.position.z -= moveSpeed;
+    }
+    if (moveBackward) {
+      camera.position.z += moveSpeed;
+    }
+    if (moveLeft) {
+      camera.position.x -= moveSpeed;
+    }
+    if (moveRight) {
+      camera.position.x += moveSpeed;
+    }
+  }
+
+  function getRandomMouthTexture() {
+    const randomIndex = Math.floor(Math.random() * mouthTextures.length);
+    return THREE.ImageUtils.loadTexture(mouthTextures[randomIndex]);
+  }
+
+  function setRandomMouthPosition() {
+    const randomX = Math.random() * 4 - 2;
+    const randomY = Math.random() * 4 - 2;
+    const randomZ = -10;
+
+    mouthMesh.position.set(randomX, randomY, randomZ);
+  }
+
+function moveRandomMouth() {
+  const moveSpeed = 0.01;
+
+  const radius = 4.5;
+  const angle = Date.now() * 0.0005;
+  const x = Math.cos(angle) * radius;
+  const y = Math.sin(angle) * radius;
+  mouthMesh.position.x = x;
+  mouthMesh.position.y = y;
+
+  if (mouthMesh.position.z > 0) {
+    setRandomMouthPosition();
+    mouthMaterial.map = getRandomMouthTexture();
+  }
+
+  if (isOpening) {
+    mouthOpenAmount += 0.05;
+    if (mouthOpenAmount >= 1) {
+      isOpening = false;
+      mouthMaterial.map = THREE.ImageUtils.loadTexture(mouthTextures[1]);
+    }
+  } else {
+    mouthOpenAmount -= 1;
+    if (mouthOpenAmount <= 0) {
+      isOpening = true;
+      mouthMaterial.map = THREE.ImageUtils.loadTexture(mouthTextures[0]);
+    }
+  }
+
+  mouthMesh.scale.set(1.2, 0.8, 1.0);
+  mouthMesh.scale.y *= Math.abs(Math.sin(mouthOpenAmount * Math.PI));
+}
+
+  setRandomMouthPosition(); // Posicionar la boca inicialmente
+  mouthMaterial.map = THREE.ImageUtils.loadTexture(mouthTextures[0]);
+
+  animate();
 }
 
 window.onload = main;
